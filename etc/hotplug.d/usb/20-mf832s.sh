3a91b13240b836a780acf0d3adc83c318cfecb33
墨焓 <zmhungrown@gmail.com>
2018-06-22 21:18:16 +0800

    Create 20-mf832s.sh

---
+++ etc/hotplug.d/usb/20-mf832s.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+MF832S_PRODUCT="19d2/199/100"
+MODEM=/dev/ttyUSB0
+NETIF=network.interface.mf832s
+
+[ "$PRODUCT" = "$MF832S_PRODUCT" ] && [ "${DEVICENAME##*.}" = "4" ] || exit
+
+[ "$ACTION" = "remove" ] &&\
+    { logger -t hotplug "MF832S: detach" &&\
+    sleep 3 &&\
+    ubus call $NETIF status | grep available | grep false &&\
+    logger -t hotplug "MF832S: $NETIF down" &&\
+    ubus call $NETIF down ||\
+    exit ; }
+
+[ "$ACTION" = "add" ] &&\
+    { logger -t hotplug "MF832S: attach" &&\
+    [ -c $MODEM ] &&\
+        { logger -t hotplug "MF832S: test" &&\
+        chat -f /etc/chatscripts/mf832s-online-test.chat <$MODEM >$MODEM &&\
+        logger -t hotplug "MF832S: online" ; } ||\
+        { logger -t hotplug "MF832S: offline" &&\
+        logger -t hotplug "MF832S: $MODEM chat" &&\
+        chat -f /etc/chatscripts/mf832s.chat <$MODEM >$MODEM &&\
+        logger -t hotplug "MF832S: $MODEM chat success" &&\
+        ubus call $NETIF status | grep available | grep true &&\
+        logger -t hotplug "MF832S: $NETIF up" &&\
+        ubus call $NETIF up ; } ||\
+        logger -t hotplug "MF832S: $MODEM chat fail" ||\
+    exit ; }
